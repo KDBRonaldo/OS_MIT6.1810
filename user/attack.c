b1fd0a4d9a6b76fe5ad88c676ec73ec4f42eb052
rate <1808369767@qq.com>
2026-05-20 10:18:39 +0800

    lab 2 : finish attack

--- user/attack.c
+++ user/attack.c
@@ -3,10 +3,20 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define DATASIZE (8*4096)
+
 int
 main(int argc, char *argv[])
 {
   // Your code here.
+  char *p = sbrk(DATASIZE);
+
+  int i;
+  for(i = 0; i < DATASIZE - 16; i++){
+    if(strcmp("This may help.", p + i) == 0){
+      printf("%s\n", p + i + 16);
+    }
+  }
 
   exit(1);
 }
